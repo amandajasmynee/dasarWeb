4e993101806fa2dd930deac7a3e39d639a00574a
Amanda Jasmyne Berliana Putri <putriamanda549@gmail.com>
2024-04-21 22:34:24 +0700

    Pertemuan 10 - Praktikum 4

--- Pertemuan 10/crud/proses.php
+++ Pertemuan 10/crud/proses.php
@@ -2,10 +2,10 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
-$nama = $_POST['nama'];
-$jenis_kelamin = $_POST['jenis_kelamin'];
-$alamat = $_POST['alamat'];
-$no_telp = $_POST['no_telp'];
+// $nama = $_POST['nama'];
+// $jenis_kelamin = $_POST['jenis_kelamin'];
+// $alamat = $_POST['alamat'];
+// $no_telp = $_POST['no_telp'];
 
 if ($aksi == 'tambah') {
     $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
@@ -31,6 +31,23 @@ if ($aksi == 'tambah') {
     } else {
         echo "ID tidak valid.";
     }
-} 
+} elseif ($aksi == 'hapus') {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM anggota WHERE id = $id";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+} else {
+    header("Location: index.php");
+}
 mysqli_close($koneksi);
 ?>
