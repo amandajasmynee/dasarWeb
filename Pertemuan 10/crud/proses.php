9333484b763f3637c5ecd489aa9de30837887851
Amanda Jasmyne Berliana Putri <putriamanda549@gmail.com>
2024-04-22 00:23:29 +0700

    Pertemuan 10 - Praktikum 5

--- Pertemuan 10/crud/proses.php
+++ Pertemuan 10/crud/proses.php
@@ -2,10 +2,10 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
-// $nama = $_POST['nama'];
-// $jenis_kelamin = $_POST['jenis_kelamin'];
-// $alamat = $_POST['alamat'];
-// $no_telp = $_POST['no_telp'];
+$nama = $_POST['nama'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$alamat = $_POST['alamat'];
+$no_telp = $_POST['no_telp'];
 
 if ($aksi == 'tambah') {
     $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
