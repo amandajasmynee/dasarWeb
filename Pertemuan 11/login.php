f3d237166d1625735543a145e0f81c0335233bb0
Amanda Jasmyne Berliana Putri <putriamanda549@gmail.com>
2024-04-22 13:28:16 +0700

    Pertemuan 11 - Praktikum 3

--- Pertemuan 11/login.php
+++ Pertemuan 11/login.php
@@ -15,7 +15,7 @@ include 'fungsi/pesan_kilat.php';
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.118.2">
-    <title>Aplikasi Kantor Chamillatul</title>
+    <title>SEVENTEEN's Office</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
 
